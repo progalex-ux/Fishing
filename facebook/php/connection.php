b60d66388ec2952052f7d959d9b907437865328f
Alejandro Reynoso <143570433+progalex-ux@users.noreply.github.com>
2024-03-20 11:27:19 -0600

    Delete facebook directory

--- facebook/php/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $connection = new PDO('mysql:host=localhost;dbname=facebook;', "root", '');
-    echo "Connection Success";
-} catch (PDOException $e) {
-    echo "error" . $e->getMessage();
-}
-?>
